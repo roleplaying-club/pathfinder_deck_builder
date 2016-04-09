b984868203a9ab2ca5a61357cfc22e3ddad302f2
Kyle Sponheim <kyle.sponheim@gmail.com>
2016-04-08 19:23:01 -0700

    Update armor_card.rb

--- lib/armor_card.rb
+++ lib/armor_card.rb
@@ -2,21 +2,37 @@ require_relative 'character_card'
 
 class ArmorCard < CharacterCard
 
-def create_armor_card(xml_file)
-    xml_file["document"]["public"]["character"]["defenses"]["armor"].each do |armor|
+  def create_card(xml_file)
+    if xml_file["document"]["public"]["character"]["defenses"]["armor"].class == Hash
       @class_cards << {
         "count": 1,
         "color": "grey",
-        "title": "#{armor["name"]}",
-        "icon": nil,
-        "contents": [
-          "property | AC | #{armor["ac"]}",
-          "property | Weight | #{armor["weight"]["text"]}",
-          "property | Cost | #{armor["cost"]["text"]}",
-          "property | Quantity | #{armor["quantity"]}",
-          "property | Description | #{armor["description"]}"
+          "title": "#{xml_file["document"]["public"]["character"]["defenses"]["armor"]["name"]}",
+          "icon": nil,
+          "contents": [
+          "property | AC | #{xml_file["document"]["public"]["character"]["defenses"]["armor"]["ac"]}",
+          "property | Weight | #{xml_file["document"]["public"]["character"]["defenses"]["armor"]["weight"]["text"]}",
+          "property | Cost | #{xml_file["document"]["public"]["character"]["defenses"]["armor"]["cost"]["text"]}",
+          "property | Quantity | #{xml_file["document"]["public"]["character"]["defenses"]["armor"]["quantity"]}",
+          "property | Description | #{xml_file["document"]["public"]["character"]["defenses"]["armor"]["description"]}"
         ]
       }
+    elsif xml_file["document"]["public"]["character"]["defenses"]["armor"].class == Array
+      xml_file["document"]["public"]["character"]["defenses"]["armor"].each do |armor|
+        @class_cards << {
+          "count": 1,
+          "color": "grey",
+          "title": "#{armor["name"]}",
+          "icon": nil,
+          "contents": [
+            "property | AC | #{armor["ac"]}",
+            "property | Weight | #{armor["weight"]["text"]}",
+            "property | Cost | #{armor["cost"]["text"]}",
+            "property | Quantity | #{armor["quantity"]}",
+            "property | Description | #{armor["description"]}"
+          ]
+        }
+      end
     end
   end
 end
