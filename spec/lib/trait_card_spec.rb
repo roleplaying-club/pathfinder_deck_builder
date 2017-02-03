ff74b729988e026427fcd67b0682a8bc10d28e58
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-02-02 16:39:54 -0800

    Add trait card spec

---
+++ spec/lib/trait_card_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../spec_helper.rb'
+require_relative '../../lib/trait_card.rb'
+require 'json'
+
+describe TraitCard do 
+  
+  before {@trait_card = TraitCard.new}
+
+  it "#static_content returns a hash " do
+    expect(@trait_card.static_content).to eq({
+      "count": 1,
+      "color": "green",
+      "title": "Trait",
+      "icon": "usable"
+    })
+  end
+end
