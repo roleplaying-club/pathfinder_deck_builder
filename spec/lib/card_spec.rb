3ebc572a27a71e65b8298c5358288f8885650bbe
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-02-14 19:02:27 -0800

    test Card#create_card

--- spec/lib/card_spec.rb
+++ spec/lib/card_spec.rb
@@ -4,22 +4,38 @@ require 'json'
 require 'crack'
 
 describe Card do 
+  before { @card = Card.new(Crack::XML.parse(File.read('spec/Aer.xml'))) }
 
-  context "with one party member" do
-    before { @card = Card.new(Crack::XML.parse(File.read('spec/Aer.xml'))) }
-    
-    it "#set_paths" do
-      expect(@card).to receive(:set_single_character_path)
-      @card.set_paths
+  describe "#create_card" do
+    it "calls #set_paths on itself" do 
+      expect(@card).to receive(:set_paths)
+      @card.create_card
+    end
+
+    it "calls #set_class_path on itself" do
+      expect(@card).to receive(:set_class_path)
+      @card.create_card
     end
   end
 
-  context "with more than one party member" do
-    before { @card = Card.new(Crack::XML.parse(File.read('spec/Fellowship.xml'))) }
-    before { @card.index = 0 }
-    it "#set_paths" do
-      expect(@card).to receive(:set_multiple_character_path)
-      @card.set_paths
+  describe "#set_paths" do
+    context "with one party member" do
+      before { @card = Card.new(Crack::XML.parse(File.read('spec/Aer.xml'))) }
+    
+      it "calls #set_single_character_path on itself" do
+        expect(@card).to receive(:set_single_character_path)
+        @card.set_paths
+      end
+    end
+
+    context "with more than one party member" do
+      before { @card = Card.new(Crack::XML.parse(File.read('spec/Fellowship.xml'))) }
+      before { @card.index = 0 }
+
+      it "calls #set_multiple_character_path on itself" do
+        expect(@card).to receive(:set_multiple_character_path)
+        @card.set_paths
+      end
     end
   end
 end
