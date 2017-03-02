a8beafc7a451f0d82ec88e06c684bf0faf3c78dc
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-03-02 07:29:48 -0800

    add specs for armor_card and character_card

---
+++ spec/lib/armor_card_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../spec_helper.rb'
+require_relative '../../lib/armor_card.rb'
+require 'json'
+require 'crack'
+
+describe ArmorCard do 
+  before {@armor_card = ArmorCard.new(Crack::XML.parse(File.read('spec/Aer.xml')))}
+
+  it "#static_content returns a hash" do
+    expect(@armor_card.static_content).to eq({
+      "count": 1,
+      "color": "grey",
+      "title": "Armor",
+      "icon": "anvil"
+    })
+  end
+end
